be61c4f35139ac38bf0ac0110b6bdcb3233f6767
Jayesh Anand <jayeshanand2002@gmail.com>
2024-11-24 16:06:03 +0100

    Added test6 folder in Jayesh which works with multiple logins simultaneously

---
+++ Jayesh/test6/db.py
@@ -0,0 +1,57 @@
+import sqlite3
+from config import Config
+
+def get_db_connection():
+    conn = sqlite3.connect(Config.DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_tables():
+    conn = get_db_connection()
+
+    # Create Customers Table
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS customers (
+            CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,
+            FirstName TEXT,
+            LastName TEXT,
+            Address TEXT,
+            ZipCode TEXT,
+            PhoneNumber TEXT UNIQUE,
+            Password TEXT,
+            Role TEXT DEFAULT 'customer',
+            Balance REAL DEFAULT 100.0,
+            CreatedAt DATETIME
+        )
+    ''')
+
+    # Create Restaurants Table
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS restaurants (
+            RestaurantID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Name TEXT,
+            Email TEXT UNIQUE,  -- Unique email for restaurant login
+            Address TEXT,
+            ZipCode TEXT,
+            Description TEXT,
+            ImageURL TEXT, -- Store the file path instead of URL
+            Password TEXT,
+            Balance REAL DEFAULT 0.0,
+            OpenTime TIME,
+            CloseTime TIME,
+            CreatedAt DATETIME
+        )
+    ''')
+
+    # Create Delevery ZIP Codes Table
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS delivery_zip_codes (
+            ZipCodeID INTEGER PRIMARY KEY AUTOINCREMENT,
+            RestaurantID INTEGER,
+            ZipCode TEXT,
+            FOREIGN KEY (RestaurantID) REFERENCES restaurants(RestaurantID)
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
