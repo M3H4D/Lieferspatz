2b2b0ccb7d9ad7c6ef6911eb83646bbec6c55db9
Jayesh Anand <jayeshanand2002@gmail.com>
2024-12-31 13:53:01 +0100

    Added the About Us page in the footer in the base.html

--- Mehad/Integration - V5/routes/home.py
+++ Mehad/Integration - V5/routes/home.py
@@ -5,3 +5,7 @@ home_bp = Blueprint('home', __name__)
 @home_bp.route('/')
 def home():
     return render_template('home.html')
+
+@home_bp.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
