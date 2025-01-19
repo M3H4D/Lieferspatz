04326f3c3b976a8ec4086e3be6a1e3c5ff2d07dc
M3H4D <mehad.shakir@hotmail.com>
2025-01-19 13:15:38 +0100

    Code Clean up

--- Mehad/Integration - V6/routes/restaurant.py
+++ Mehad/Integration - V6/routes/restaurant.py
@@ -164,11 +164,6 @@ def restaurant_additems():
     restaurant_data = session['restaurant']
     return render_template('add_item.html', user=restaurant_data)
 
-# @restaurant_bp.route('/delete_item', methods=['GET', 'POST'])
-# def restaurant_delete_item():
-#     RDB_util.delete_item_from_database(request.form['ItemID'])
-#     return redirect(url_for('restaurant.restaurant_dashboard'))
-
 @restaurant_bp.route('/edit_item_screen', methods=['GET', 'POST'])
 def restaurant_edit_item_screen():
     # clicking edit on html page retrieves the ItemID and then renders the edit item page
@@ -220,8 +215,6 @@ def restaurant_edit_item():
         flash('Item Edited!', 'success')
 
     return redirect(url_for('restaurant.restaurant_dashboard'))
-
-
 
 @restaurant_bp.route('/restaurant/received_orders')
 def received_orders():
