237dce8d07de57f6d898b445da32e4ec67e0b94a
M3H4D <mehad.shakir@hotmail.com>
2025-01-26 12:12:27 +0100

    Removed redundant code

--- Mehad/Integration - V6.2/routes/customer.py
+++ Mehad/Integration - V6.2/routes/customer.py
@@ -353,25 +353,6 @@ def past_orders():
     previousorders = [dict(order) for order in previousorders]
     for order in previousorders:
         order['CreatedAt'] = datetime.strptime(order['CreatedAt'], '%Y-%m-%d %H:%M:%S.%f')
-
-    # Fetch items for each order
-    for order in currentorders:
-        cursor.execute('''
-            SELECT i.Name, oi.Quantity
-            FROM OrderItems oi
-            JOIN items i ON oi.ItemID = i.ItemID
-            WHERE oi.OrderID = ?
-        ''', (order['OrderID'],))
-        order['Items'] = cursor.fetchall()
-
-    for order2 in previousorders:
-        cursor.execute('''
-            SELECT i.Name, oi.Quantity
-            FROM OrderItems oi
-            JOIN items i ON oi.ItemID = i.ItemID
-            WHERE oi.OrderID = ?
-        ''', (order2['OrderID'],))
-        order2['Items'] = cursor.fetchall()
     
     conn.close()
     return render_template('past_orders.html', currentorders=currentorders, previousorders=previousorders)
--- Mehad/Integration - V6.2/routes/restaurant.py
+++ Mehad/Integration - V6.2/routes/restaurant.py
@@ -270,34 +270,6 @@ def received_orders():
     for order in rejectedorders:
         order['CreatedAt'] = datetime.strptime(order['CreatedAt'], '%Y-%m-%d %H:%M:%S.%f')
 
-    # Fetch items for each order
-    for order in currentorders:
-        cursor.execute('''
-            SELECT i.Name, oi.Quantity
-            FROM OrderItems oi
-            JOIN items i ON oi.ItemID = i.ItemID
-            WHERE oi.OrderID = ?
-        ''', (order['OrderID'],))
-        order['Items'] = cursor.fetchall()
-
-    for order2 in completedorders:
-        cursor.execute('''
-            SELECT i.Name, oi.Quantity
-            FROM OrderItems oi
-            JOIN items i ON oi.ItemID = i.ItemID
-            WHERE oi.OrderID = ?
-        ''', (order2['OrderID'],))
-        order2['Items'] = cursor.fetchall()
-
-    for order3 in rejectedorders:
-        cursor.execute('''
-            SELECT i.Name, oi.Quantity
-            FROM OrderItems oi
-            JOIN items i ON oi.ItemID = i.ItemID
-            WHERE oi.OrderID = ?
-        ''', (order3['OrderID'],))
-        order3['Items'] = cursor.fetchall()
-
     conn.close()
 
     restaurant_data = session['restaurant']
@@ -394,6 +366,7 @@ def edit_restaurant():
 @restaurant_bp.route('/delete_item', methods=['POST'])
 def restaurant_delete_item():
     item_id = request.form['ItemID']
+    print(item_id)
     RDB_util.delete_item_from_database(item_id)
     flash('Item Deleted!', 'success')
     return redirect(url_for('restaurant.restaurant_dashboard'))
