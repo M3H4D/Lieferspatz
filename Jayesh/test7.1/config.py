887f93a2c99c52a7a90da16ddaa6612fdf87037b
M3H4D <mehad.shakir@hotmail.com>
2026-06-02 21:42:31 +0200

    Cleaning up directory

--- Jayesh/test7.1/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'supersecretkey'
-    DATABASE = 'lieferspatz.db'
-    UPLOAD_FOLDER = 'static/images'
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
--- Mehad/Integration - V2/static/js/addZipCodeField.js
+++
@@ -1,8 +0,0 @@
-function addZipCode() {
-    const container = document.getElementById('zipCodeContainer');
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.name = 'delivery_zip_codes';
-    input.placeholder = 'Enter delivery zip code';
-    container.appendChild(input);
-}
